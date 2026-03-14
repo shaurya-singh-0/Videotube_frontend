2518964dbba940fb090f9b225f7b374ad08996f8
SloneAoul <shorya16705@gmail.com>
2026-03-14 21:33:39 +0530

    update baseURL to production

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "https://videotube-frontend-beta.vercel.app",
+    baseURL: "https://videotube-backend-s8ks.onrender.com/api/v1",
     withCredentials: true  // important for cookies/JWT
 })
 
